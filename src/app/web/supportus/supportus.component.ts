44d93f5896bf218b8891d7137e03e9ceb8143027
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-06-29 16:38:05 +0300

    added supportus page and made it's route in navbar

--- src/app/web/supportus/supportus.component.ts
+++ src/app/web/supportus/supportus.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { WebNavbarComponent } from '../../shared/web-navbar/web-navbar.component';
+import { WebFooterComponent } from '../../shared/web-footer/web-footer.component';
 
 @Component({
   selector: 'app-supportus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, WebNavbarComponent, WebFooterComponent],
   templateUrl: './supportus.component.html',
   styleUrl: './supportus.component.css'
 })
