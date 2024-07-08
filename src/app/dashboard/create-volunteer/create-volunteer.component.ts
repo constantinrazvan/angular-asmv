e4f3276f5f354037351543aeadc5532e59b6a312
constantinrazvan <76180811+constantinrazvan@users.noreply.github.com>
2024-07-09 01:46:35 +0300

    added pages for creating volunteer, blog and project but not built it

---
+++ src/app/dashboard/create-volunteer/create-volunteer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-volunteer',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-volunteer.component.html',
+  styleUrl: './create-volunteer.component.css'
+})
+export class CreateVolunteerComponent {
+
+}
